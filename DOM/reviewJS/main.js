fbad2b2c48857d41d76d48e125efb2db0f1c0f1a
amdymstfa <amdymoustapha011@gmail.com>
2025-01-06 15:15:04 +0100

    done : innerHTML, textContent, classList

---
+++ DOM/reviewJS/main.js
@@ -0,0 +1,15 @@
+// recupere tout les balises articles de la page web
+// const articles = document.getElementsByTagName('article');
+// recuperer les classes 
+// const paragraphs = document.getElementsByTagName('p');
+
+// const fArticle = articles[0];
+// const fParagraph = paragraphs[0] ;
+
+// recuperer un element a partir de la classe
+
+// const whiteText = document.getElementsByClassName('text-white') ;
+
+const mainHeading = document.getElementById('main-heading') ;
+// mainHeading.textContent = 'Modify the DOM';
+mainHeading.innerHTML = '<h2>Modify the DOM</h2>';
